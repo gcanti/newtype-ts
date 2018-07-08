a661dacd660c48161518e5f0af550e9d1219638b
gcanti <giulio.canti@gmail.com>
2018-07-08 12:23:29 +0200

    Deprecation: deprecate Carrier in favour of CarrierOf

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,50 @@
-import { Iso, Getter } from 'monocle-ts'
-import { Setoid } from 'fp-ts/lib/Setoid'
-import { Ord } from 'fp-ts/lib/Ord'
+import { Field } from 'fp-ts/lib/Field'
 import { Monoid } from 'fp-ts/lib/Monoid'
+import { Ord } from 'fp-ts/lib/Ord'
+import { Ring } from 'fp-ts/lib/Ring'
 import { Semigroup } from 'fp-ts/lib/Semigroup'
 import { Semiring } from 'fp-ts/lib/Semiring'
-import { Ring } from 'fp-ts/lib/Ring'
-import { Field } from 'fp-ts/lib/Field'
-
-export const unsafeCoerce = <A, B>(a: A): B => a as any
+import { Setoid } from 'fp-ts/lib/Setoid'
+import { Getter, Iso } from 'monocle-ts'
 
 export interface Newtype<URI, A> {
   _URI: URI
   _A: A
 }
 
+export type CarrierOf<N extends Newtype<any, any>> = N['_A']
+
+/**
+ * @deprecated use `CarrierOf` instead
+ */
 export type Carrier<N extends Newtype<any, any>> = N['_A']
 
 export type AnyNewtype = Newtype<any, any>
 
-const anyIso = new Iso<any, any>(unsafeCoerce, unsafeCoerce)
-
-export const iso = <S extends AnyNewtype>(): Iso<S, Carrier<S>> => anyIso
-
 /** Lifts a function operate over newtypes */
-export const over = <S extends AnyNewtype, T extends AnyNewtype>(f: (a: Carrier<S>) => Carrier<T>): Getter<S, T> =>
+export const over = <S extends AnyNewtype, T extends AnyNewtype>(f: (a: CarrierOf<S>) => CarrierOf<T>): Getter<S, T> =>
   new Getter(f)
 
-export const getSetoid = <S extends AnyNewtype>(S: Setoid<Carrier<S>>): Setoid<S> => S
+export const getSetoid = <S extends AnyNewtype>(S: Setoid<CarrierOf<S>>): Setoid<S> => S
 
-export const getOrd = <S extends AnyNewtype>(O: Ord<Carrier<S>>): Ord<S> => O
+export const getOrd = <S extends AnyNewtype>(O: Ord<CarrierOf<S>>): Ord<S> => O
 
-export const getSemigroup = <S extends AnyNewtype>(S: Semigroup<Carrier<S>>): Semigroup<S> => S
+export const getSemigroup = <S extends AnyNewtype>(S: Semigroup<CarrierOf<S>>): Semigroup<S> => S
 
-export const getMonoid = <S extends AnyNewtype>(M: Monoid<Carrier<S>>): Monoid<S> => M
+export const getMonoid = <S extends AnyNewtype>(M: Monoid<CarrierOf<S>>): Monoid<S> => M
 
-export const getSemiring = <S extends AnyNewtype>(S: Semiring<Carrier<S>>): Semiring<S> => S
+export const getSemiring = <S extends AnyNewtype>(S: Semiring<CarrierOf<S>>): Semiring<S> => S
 
-export const getRing = <S extends AnyNewtype>(R: Ring<Carrier<S>>): Ring<S> => R
+export const getRing = <S extends AnyNewtype>(R: Ring<CarrierOf<S>>): Ring<S> => R
 
-export const getField = <S extends AnyNewtype>(F: Field<Carrier<S>>): Field<S> => F
+export const getField = <S extends AnyNewtype>(F: Field<CarrierOf<S>>): Field<S> => F
+
+//
+// isomorphisms
+//
+
+export const unsafeCoerce = <A, B>(a: A): B => a as any
+
+const anyIso = new Iso<any, any>(unsafeCoerce, unsafeCoerce)
+
+export const iso = <S extends AnyNewtype>(): Iso<S, CarrierOf<S>> => anyIso
