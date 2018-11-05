ddf79532cd45c7922c077a158b1e98429a70484c
gcanti <giulio.canti@gmail.com>
2018-11-05 15:29:47 +0100

    New Feature: add NonEmptyString, closes #13

---
+++ test/NontEmptyString.ts
@@ -0,0 +1,10 @@
+import * as assert from 'assert'
+import { none, some } from 'fp-ts/lib/Option'
+import { prismNonEmptyString } from '../src/NonEmptyString'
+
+describe('NonEmptyString', () => {
+  it('prismNonEmptyString', () => {
+    assert.deepEqual(prismNonEmptyString.getOption('a'), some('a'))
+    assert.deepEqual(prismNonEmptyString.getOption(''), none)
+  })
+})
